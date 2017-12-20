dba473dca8ce44646f997a46a130ef9e60208d10
amaury.lekens <14027@student.ecam.be>
2017-12-20 17:01:49 +0100

    finish

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from neo4j.v1 import GraphDatabase, basic_auth
-import re
+from time import time, mktime
+from datetime import datetime, timedelta
 
 
 class Database :
@@ -18,81 +19,92 @@ class Database :
             raise Exception("This class is a singleton!")
         else:
             Database.__instance = self
-            driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "neo4j"))
-            self.__session = driver.session()
+            self.__driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth('amaury', 'amaury'))
 
 
     def addUser (self, username, password):
         request = "CREATE (:User {username : '" + username + "', password : '" + password + "'})"
-        self.__session.run(request)
+        session = self.__driver.session()
+        session.run(request)
 
     def addTeam (self, name) :
         request = "CREATE (:Team {name : '" + name + "'})"
-        self.__session.run(request)
+        session = self.__driver.session()
+        session.run(request)
 
-    def addVideo (self, homeTeam, awayTeam, score, link) :
-        goals = re.match("([0-9]+) ?- ?([0-9]+)", score)
-        goalsHome = goals.group(1)
-        goalsAway = goals.group(2)
-
+    def addVideo (self, homeTeam, awayTeam, goalsHome, goalsAway, link) :
         #create video entity
-        request = "CREATE (:Video {goalsHome : '" + goalsHome + "', goalsAway : '" + goalsAway \
-                  + "', link : '" + link + "'})"
-        self.__session.run(request)
+        date = str(int(time()))
+        request = "CREATE (:Video {link : '" + link + "', goalsHome : '" + goalsHome \
+                  + "', goalsAway : '" + goalsAway + "', homeTeam : '" + homeTeam + \
+                  "', awayTeam : '" + awayTeam + "', date : " + date + "})"
+        session = self.__driver.session()
+        session.run(request)
 
-        #check if teams entity exist. If doesn't, create it
-        request = "MATCH (t:Team {name : {name}})) RETURN t"
         teams = [homeTeam, awayTeam]
-
-        for t in teams :
-            result = self.__session.run(request, {'name' : t})
-            try :
-                t = result.single()
-                print(t['t'].properties['name'], "déja créée")
-            except :
-                self.addTeam(t)
 
         #create link
         for t in teams :
-            request = "CREATE (:Video {link: '" + link + "'}) -[:CONCERNS]-> (:Team {name : '" + t + "'})"
-            self.__session.run(request)
+            request = "MATCH (v:Video {link: '" + link + "'}) \
+                       MATCH (t:Team {name : '" + t + "'})     \
+                      CREATE (v) -[:CONCERNS]-> (t)"
+            session = self.__driver.session()
+            session.run(request)
+
+    def getTeams(self):
+        request = "MATCH (t:Team) RETURN t"
+        session = self.__driver.session()
+        result = session.run(request)
+
+        teams = []
+        for team in result.records():
+            teams.append(team['t'].properties['name'])
+
+        return teams
+
 
     def addViewLinks (self, link, username):
         #create link user --> video, but before check if the link already exist
         request = "MATCH (:User {username : '" + username + "'})-[w:WATCH]-> " \
-                                                        "(:Video {link: '" + link + "'}) SET w.number = w.number + 1"
+                                         "(:Video {link: '" + link + "'}) SET w.number = w.number + 1 RETURN w"
+        session = self.__driver.session()
+        result = session.run(request)
 
-        try:
-            self.__session.run(request)
-        except:
-            request = "CREATE (:User {username : '" + username + "'})-[:WATCH {number : 1}]->" \
-                                                                     "(:Video {link: '" + link + "'})"
-            self.__session.run(request)
+        if result.single() == None:
+            print("hello")
+            request = "MATCH (u:User {username : '" + username + "'})\
+                       MATCH (v:Video {link: '" + link + "'})\
+                       CREATE (u)-[:WATCH {number : 1}]->(v)"
+            session = self.__driver.session()
+            session.run(request)
 
         #look to the concerned teams
-        request = "MATCH (:Video {link : '" + link + "'}) -[CONCERNS]-> (t:Team) RETURN t.name"
-        result = self.__session.run(request)
+        request = "MATCH (:Video {link : '" + link + "'}) -[CONCERNS]-> (t:Team) RETURN t"
+        session = self.__driver.session()
+        result = session.run(request)
 
         teams = []
         for team in result.records():
-            teams.append(team)
+            teams.append(team['t'].properties['name'])
 
         #create the link user --> team, but before check if the link already exist
         for t in teams :
             # create link user --> video, but before check if the link already exist
             request = "MATCH (:User {username : '" + username + "'})-[w:WATCH]->(:Team {name: '" + t + "'}) " \
-                                                                                        "SET w.number = w.number + 1"
-
-            try:
-                self.__session.run(request)
-            except:
-                request = "CREATE (:User {username : '" + username + "'})-[:WATCH {number : 1}]->" \
-                                                                     "(:Team {name: '" + t + "'})"
-                self.__session.run(request)
+                                                                    "SET w.number = w.number + 1 RETURN w"
+            session = self.__driver.session()
+            result = session.run(request)
+            if result.single() == None:
+                request = "MATCH (u:User {username : '" + username + "'})\
+                           MATCH (t:Team {name: '" + t + "'})\
+                           CREATE (u)-[:WATCH {number : 1}]->(t)"
+                session = self.__driver.session()
+                session.run(request)
 
     def getPassword(self,username):
         request = "MATCH (u:User {username : '" + username + "'}) RETURN u"
-        result = self.__session.run(request)
+        session = self.__driver.session()
+        result = session.run(request)
         try :
             u = result.single()
             password = u['u'].properties['password']
@@ -100,6 +112,135 @@ class Database :
         except :
             return -1
 
+    def getAllVideos(self):
+        request = "MATCH (v:Video) RETURN v"
+        session = self.__driver.session()
+        result = session.run(request)
+
+        videos = []
+        for v in result.records():
+            video = {}
+            video['link'] = v['v'].properties['link']
+            video['goalsHome'] = v['v'].properties['goalsHome']
+            video['goalsAway'] = v['v'].properties['goalsAway']
+            video['teamHome'] = v['v'].properties['homeTeam']
+            video['teamAway']= v['v'].properties['awayTeam']
+            videos.append(video)
+
+        return videos
+
+    def getTrendVideos(self):
+        dateFiveDays = (datetime(datetime.now().year, datetime.now().month, datetime.now().day)-timedelta(3)).timestamp()
+        request =  "MATCH (v:Video) "
+        request += "WHERE v.date>" + str(int(dateFiveDays)) + " "
+        request += "MATCH (:User)-[w:WATCH]->(v)"
+        request += "RETURN v, sum(w.number) AS n"
+        session = self.__driver.session()
+        result = session.run(request)
+        videos= []
+        for video in result.records():
+            videos.append((video['n'], video['v']))
+        videos = sorted(videos, key=lambda colon: colon[0])
+        videos = list(reversed(videos))
+
+        videosSelected = []
+        try :
+            for i in range(5):
+                video = {}
+                video['link'] = videos[i][1].properties['link']
+                video['goalsHome'] = videos[i][1].properties['goalsHome']
+                video['goalsAway'] = videos[i][1].properties['goalsAway']
+                video['teamHome'] = videos[i][1].properties['homeTeam']
+                video['teamAway'] = videos[i][1].properties['awayTeam']
+                videosSelected.append(video)
+        except IndexError:
+            pass
+
+        return videosSelected
+
+
+    def getNextVideos(self, teams, username):
+        numbersOfViews = {}
+        for team in teams :
+            request = "MATCH (:User {username:'" + username + "'})-[w:WATCH]->(:Team {name : '" + team + "'}) RETURN w"
+            session = self.__driver.session()
+            result = session.run(request)
+            w = result.single()
+            numbersOfViews[team] = w['w'].properties['number']
+
+        favoriteTeam = ""
+        max = 0
+        for team in teams :
+            if numbersOfViews[team] > max :
+                max = numbersOfViews[team]
+                favoriteTeam = team
+
+        request = "MATCH (v:Video)-[:CONCERNS]->(:Team {name : '" + favoriteTeam + "'}) "
+        request += "MATCH (:User)-[w:WATCH]->(v) "
+        request += "RETURN v, sum(w.number) AS n"
+        session = self.__driver.session()
+        result = session.run(request)
+        videos = []
+        for video in result.records():
+            videos.append((video['n'], video['v']))
+        videos = sorted(videos, key=lambda colon: colon[0])
+        videos = list(reversed(videos))
+
+        videosSelected = []
+        try:
+            for i in range(5):
+                video = {}
+                video['link'] = videos[i][1].properties['link']
+                video['goalsHome'] = videos[i][1].properties['goalsHome']
+                video['goalsAway'] = videos[i][1].properties['goalsAway']
+                video['teamHome'] = videos[i][1].properties['homeTeam']
+                video['teamAway'] = videos[i][1].properties['awayTeam']
+                videosSelected.append(video)
+        except IndexError:
+            pass
+
+        return videosSelected
+
+    def getFavoriteTeamVideos(self, username):
+        request = "MATCH (:User {username:'" + username + "'})-[w:WATCH]->(t:Team) RETURN w.number AS w, t.name AS t"
+        session = self.__driver.session()
+        result = session.run(request)
+        favoriteTeam = ""
+
+        max = 0
+        for team in result.records():
+            if team['w'] > max:
+                max = team['w']
+                favoriteTeam = team['t']
+
+        request = "MATCH (v:Video)-[:CONCERNS]->(:Team {name : '" + favoriteTeam + "'}) "
+        request += "MATCH (:User)-[w:WATCH]->(v) "
+        request += "RETURN v, sum(w.number) AS n"
+        session = self.__driver.session()
+        result = session.run(request)
+        videos = []
+        for video in result.records():
+            videos.append((video['n'], video['v']))
+        videos = sorted(videos, key=lambda colon: colon[0])
+        videos = list(reversed(videos))
+
+        videosSelected = []
+        try:
+            for i in range(5):
+                video = {}
+                video['link'] = videos[i][1].properties['link']
+                video['goalsHome'] = videos[i][1].properties['goalsHome']
+                video['goalsAway'] = videos[i][1].properties['goalsAway']
+                video['teamHome'] = videos[i][1].properties['homeTeam']
+                video['teamAway'] = videos[i][1].properties['awayTeam']
+                videosSelected.append(video)
+        except IndexError:
+            pass
+
+        return videosSelected
+
+
     def close(self):
-        self.__session.close()
+        session = self.__driver.session()
+        session.close()
 
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from database import Database
 
 app = Flask(__name__)
-#database = Database.getInstance()
+app.secret_key = 'You Will Never Guess'
+database = Database.getInstance()
 
 @app.route('/')
 def login():
@@ -16,24 +17,63 @@ def register():
 def checklogin():
     username = request.form['username']
     password = request.form['password']
-    #realPasseword = database.getPassword(username)
+    realPasseword = database.getPassword(username)
 
-    """if password == realPasseword :
+    if password == realPasseword :
+        session['username'] = username
+        session['nextVideosTeam'] = []
         return redirect('/home')
     else :
-        return redirect('/')"""
+        return redirect('/')
 
 @app.route('/saveregister', methods=['POST'])
 def saveregister():
     username = request.form['username']
     password = request.form['password']
-    #database.addUser(username, password)
+    database.addUser(username, password)
     return redirect('/')
 
 
 @app.route('/home')
 def home():
-    return render_template("videogallery.html")
+    nextVideos=[]
+    if(session['nextVideosTeam'] != []):
+        nextVideos = database.getNextVideos(session['nextVideosTeam'], session['username'])
+    trendVideos = database.getTrendVideos()
+    favoriteTeamVideos = database.getFavoriteTeamVideos(session['username'])
+    allVideos = database.getAllVideos()
+    return render_template("videogallery.html", trendVideos=trendVideos, allVideos=allVideos,
+                           nextVideos=nextVideos, favoriteTeamVideos=favoriteTeamVideos)
+
+@app.route('/admin', methods=['POST', 'GET'])
+def admin():
+    if isset('addTeam'):
+        name = request.form['name']
+        database.addTeam(name)
+    elif isset('addVideo'):
+        teamHome = request.form['team_home']
+        teamAway = request.form['team_away']
+        goalsHome = request.form['goals_home']
+        goalsAway = request.form['goals_away']
+        link = request.form['link']
+        database.addVideo(teamHome, teamAway, goalsHome, goalsAway, link)
+    teams = database.getTeams()
+    return render_template("admin.html", teams=teams)
+
+@app.route('/video', methods=['POST'])
+def video():
+    database.addViewLinks(request.form['link'], session['username'])
+    session['nextVideosTeam'] = [request.form['teamHome'], request.form['teamAway']]
+    return redirect('//www.youtube.com/embed/'+request.form['link'])
+
+
+
+
+def isset(name):
+    for elem in request.form:
+        if elem == name :
+            return True
+    return False
 
 
 
